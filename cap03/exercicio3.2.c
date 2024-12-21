2daa14d8970f2056bc4cc024a26c090cb5406109
luiz fernando dos passos <luizsilva.miui@gmail.com>
2024-12-20 23:44:36 -0300

    cap 03

---
+++ cap03/exercicio3.2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (void) {
+    int i;
+
+    for (i = 0; i <= 50; i++) {
+        if(i % 2 == 0){
+            printf("%d ", i) ;
+        }
+        
+
+    }
+}
